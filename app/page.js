afd1cbec6f25d7a4678b990e7514895f5ba0027c
vishaldhangar15 <vsdhangar_b20@el.vjti.ac.in>
2023-03-02 13:37:39 +0530

    navbar initial commit

--- app/page.js
+++ app/page.js
@@ -1,7 +1,13 @@
+import Navbar from "../frontendComponent/Navbar/Navbar"
+
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 export default function Home() {
   return (
     <main>
-      <h1 className='text-4xl text-pink-500'>Next.js + Google Fonts</h1>
+      {/* <h1 className='text-4xl text-pink-500'>Next.js + Google Fonts</h1>   */}
+      <Navbar />
+
+
     </main>
   )
 }
