99d88bf59ef646780761600dd6d201d93aa0776b
Saurabh Sen <sensaurabh1212@gmail.com>
2023-03-10 21:52:15 +0530

    work distributed

--- app/About/Page.jsx
+++ app/About/Page.jsx
@@ -1,8 +1,17 @@
+import Footer from '@/frontendComponent/Footer'
+import NavBar from '@/frontendComponent/Navbar'
 import React from 'react'
 
 const about = () => {
   return (
-    <div>about</div>
+    <section className="about__page">
+      <NavBar />
+      <div className="about__container">
+        {/* vishal code goes here */}
+        <h1>About Page</h1>
+      </div>
+      <Footer footerbg1={"gray-700"} footerbg2={"gray-800"}/>
+    </section>
   )
 }
 
