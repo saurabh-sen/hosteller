1434bba2b66a9dabe34fafaec4194cde8a6d15bf
Deepak Mishra <deepmishradm12357@gmail.com>
2023-03-14 18:04:21 +0530

    Issue Resolved

--- backendComponent/helperfunctions/connecttodb.js
+++ backendComponent/helperfunctions/connecttodb.js
@@ -1,10 +1,12 @@
+import clientPromise from "../connection";
 export const connectFunction = async () => {
     try {
       const client = await clientPromise;
       const db = await client.db("Hosteller_DB")
       const collection = await db.collection("general");
       const myDoc = await collection.findOne({connect:'true'});
-      if(myDoc.connect==='true') {
+      console.log(myDoc.connect);
+      if(myDoc.connect=='true') {
         console.log("!!!! Database Successfully Connected !!!!");
       }
       return {
